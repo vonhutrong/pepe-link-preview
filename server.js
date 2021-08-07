061b8d463da76e517cda016d0e797bcd5513c043
vonhutrong <vonhutrong@gmail.com>
2021-08-07 14:47:23 +0700

    remove sample code in home page

--- server.js
+++ server.js
@@ -7,16 +7,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
-  var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-    { name: 'Tux', organization: "Linux", birth_year: 1996},
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-  ];
-  var tagline = "No programming concept is complete without a cute animal mascot.";
-
   res.render('pages/index', {
-    mascots: mascots,
-    tagline: tagline,
     emojis: pepe
   });
 });
