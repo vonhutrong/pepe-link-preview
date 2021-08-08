86f9c21b2917400fdea3c7273f4edb19d7e9fa73
vonhutrong <vonhutrong@gmail.com>
2021-08-08 16:35:39 +0700

    Basic home page and detail page
    On the home page, list all memes. To copy a link to a meme, just click on it.
    The detail page has metadata to achieve the link preview feature.

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var app = express();
+const pepe = require('./src/emoji/pepe');
 
 //app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
-  var mascots = [
-    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
-    { name: 'Tux', organization: "Linux", birth_year: 1996},
-    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
-  ];
-  var tagline = "No programming concept is complete without a cute animal mascot.";
-
   res.render('pages/index', {
-    mascots: mascots,
-    tagline: tagline
+    emojis: pepe
   });
+});
+
+app.get('/:meme', function(req , res){
+  res.render('pages/meme', {
+    meme: req.params.meme,
+    link: pepe[req.params.meme]}
+  );
 });
 
 app.get('/about', function(req, res) {
